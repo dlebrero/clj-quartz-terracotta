6226d8fa61e070a3543f8c023c40fd28a0467c9b
Daniel Lebrero <dlebrero@gmail.com>
2012-10-17 20:44:23 +0100

    not depending on quartize

--- src/clj_quartz_terracotta/core.clj
+++ src/clj_quartz_terracotta/core.clj
@@ -1,23 +1,20 @@
 (ns clj-quartz-terracotta.core
-  (:require [clojurewerkz.quartzite.jobs :as j]
-            [clojurewerkz.quartzite.conversion :as conv])
-  (:import org.quartz.JobBuilder)
-  (:use [clojurewerkz.quartzite.stateful]))
+  (:import [org.quartz JobBuilder JobDataMap]))
 
 (defn require-and-resolve [symbol-str]
   (let [[n h] (map symbol ((juxt namespace name) (symbol symbol-str)))]
     (require n)
     (ns-resolve (the-ns n) h)))
 
-(def-stateful-job RunAFunction
-  [ctx]
-  (println (conv/from-job-data ctx))
-  (let [all (conv/from-job-data ctx)
-        f (require-and-resolve (get all "fn-to-call"))
-        args (get all "args")]
-    (apply f args)))
+(defrecord RunAFunction []
+  org.quartz.StatefulJob
+  (execute [this ctx]
+    (let [all (.getMergedJobDataMap ctx)
+          f (require-and-resolve (get all "fn-to-call"))
+          args (get all "args")]
+      (apply f args))))
 
 (defn ^JobBuilder fn-call [^JobBuilder jb ^clojure.lang.Symbol fn-to-call & args]
   (-> jb
-    (j/of-type RunAFunction)
-    (j/using-job-data {"fn-to-call" (str fn-to-call) "args" args})))
+    (.ofType RunAFunction)
+    (.usingJobData (JobDataMap. {"fn-to-call" (str fn-to-call) "args" args}))))
