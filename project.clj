cc83cf1b1bd97c01d3ffa8b13125e46d2eed3ed4
Daniel Lebrero <dlebrero@gmail.com>
2012-10-18 23:01:33 +0100

    adding url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clj-quartz-terracotta "0.2.0-SNAPSHOT"
   :description "Some utils for running Quartz in Terracotta"
+  :url "http://github.com/dlebrero/clj-quartz-terracotta"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
